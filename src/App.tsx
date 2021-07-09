69fb79db773fd56be39322015dda26faeffd6abb
Trong Luan Tran <luan.tran@advancednavigation.com>
2021-07-09 10:27:28 +1000

    Modifying gimbal protobuf names for gimbal control

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import {Client} from '@stomp/stompjs';
 
 enum CommandType {
     NAVIGATION,
-    ARM
+    ARM,
+    GIMBAL_CONTROL
 };
 
 class App extends Component<{}, { connected: boolean }> {
@@ -19,6 +20,12 @@ class App extends Component<{}, { connected: boolean }> {
 
     manualControlProtobuf: any;
     vehiclePositionProtobuf: any;
+    operationProtobuf: any;
+
+    jwtTokenInputRef: any;
+
+    vehicleId: String = "luant-drone"; // "1a4ec7e5f7ce2d32ff125f5a57d52dbcb747ca8f7f17c514370584464dfe8df1";//demo_px4_drone_0001
+    // vehicleId: String = "329bc8a162ae7208ae18976a9c4f95e9344b9ede18ec4126224b199e39dc1156"
 
 
     constructor(props: any) {
@@ -27,6 +34,7 @@ class App extends Component<{}, { connected: boolean }> {
         this.gamepadRef = React.createRef();
         this.connectButtonRef = React.createRef();
         this.disconnectButtonRef = React.createRef();
+        this.jwtTokenInputRef = React.createRef();
         this.state = {
             connected: false
         };
@@ -40,22 +48,31 @@ class App extends Component<{}, { connected: boolean }> {
 
             // example code
             this.manualControlProtobuf = root.lookupType('ManualControl');
-            this.vehiclePositionProtobuf = root.lookupType('VehiclePosition')
+            this.vehiclePositionProtobuf = root.lookupType('VehiclePosition');
+            this.operationProtobuf = root.lookupType('Operation');
         });
     }
 
     connect = () => {
 
         console.log("App.tsx connected websocket")
-        const jwtToken = 'eyJhbGciOiJFUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICItZTQ2Tm5iTXNRbG03ZjdUc0U2Rl9uSUkzakFOQ0NvN0ttNDJPMGFXdUxFIn0.eyJleHAiOjE2MjAzOTYyNTUsImlhdCI6MTYyMDM3NTMwMCwiYXV0aF90aW1lIjoxNjIwMzY3NDU1LCJqdGkiOiJjZDBkNmUyOC01NjJiLTQzNGEtYjY3NS1mNzY0M2YxMmY3NGYiLCJpc3MiOiJodHRwczovL2FwYWMuY2xvdWRncm91bmRjb250cm9sLmNvbS9hdXRoL3JlYWxtcy9jZ2NzLWRldiIsImF1ZCI6WyJicm9rZXIiLCJjbG91ZGdyb3VuZGNvbnRyb2wtZGV2Il0sInN1YiI6ImRjZmNjYjlhLTk3NTktNGVjYy1hZWQ5LTA5ZGI2YmI0NTk3MyIsInR5cCI6IkJlYXJlciIsImF6cCI6ImNsb3VkZ3JvdW5kY29udHJvbC1zdGciLCJzZXNzaW9uX3N0YXRlIjoiYjNhYjE4NjAtM2Q2My00NDY4LWFjNzItM2Q5Mzk0MmJhYWYyIiwiYWNyIjoiMCIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sInJlc291cmNlX2FjY2VzcyI6eyJicm9rZXIiOnsicm9sZXMiOlsicmVhZC10b2tlbiJdfSwiY2xvdWRncm91bmRjb250cm9sLWRldiI6eyJyb2xlcyI6WyJVc2VyIEFkbWluaXN0cmF0aW9uIl19LCJjbG91ZGdyb3VuZGNvbnRyb2wtc3RnIjp7InJvbGVzIjpbIlVzZXIgQWRtaW5pc3RyYXRpb24iXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5hbWUiOiJMdWFuIFRyYW4iLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJsdWFuLnRyYW5AYWR2YW5jZWRuYXZpZ2F0aW9uLmNvbSIsImdpdmVuX25hbWUiOiJMdWFuIiwiZmFtaWx5X25hbWUiOiJUcmFuIiwiZW1haWwiOiJsdWFuLnRyYW5AYWR2YW5jZWRuYXZpZ2F0aW9uLmNvbSJ9.Ae9rEYAkj7mppp-s19Gy4zJnSHgBfFH3-_gqvrRZqdpP-Hy7ix7eteFRdopfrfmM-Sx7ZuTHnPoTG8-exxp_dBm2AYWEzD2soUYNSLo4gonmH1oJkgO5KTX4Juj1HUpE75AfUhz7S-WLEhDHNf1PwS0vUb9OmJ0wcuhpYzQnpw0f8U-h';
+
+        var jwtToken =  this.jwtTokenInputRef.current.value;
+        // const jwtToken = 'eyJhbGciOiJFUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICItZTQ2Tm5iTXNRbG03ZjdUc0U2Rl9uSUkzakFOQ0NvN0ttNDJPMGFXdUxFIn0.eyJleHAiOjE2MjA3MjEwOTMsImlhdCI6MTYyMDY5MjI5NSwiYXV0aF90aW1lIjoxNjIwNjkyMjkzLCJqdGkiOiJhZjEzZTQxNy1jMmQwLTQwZDQtOGVjZi1lMzc1ZjQyMDE0ODAiLCJpc3MiOiJodHRwczovL2FwYWMuY2xvdWRncm91bmRjb250cm9sLmNvbS9hdXRoL3JlYWxtcy9jZ2NzLWRldiIsImF1ZCI6WyJicm9rZXIiLCJjbG91ZGdyb3VuZGNvbnRyb2wtZGV2Il0sInN1YiI6ImRjZmNjYjlhLTk3NTktNGVjYy1hZWQ5LTA5ZGI2YmI0NTk3MyIsInR5cCI6IkJlYXJlciIsImF6cCI6ImNsb3VkZ3JvdW5kY29udHJvbC1zdGciLCJzZXNzaW9uX3N0YXRlIjoiZDUzZThkYTUtMTAzYS00OTI3LWI2YjEtYmIwNDY1ZmQ5YmFiIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sInJlc291cmNlX2FjY2VzcyI6eyJicm9rZXIiOnsicm9sZXMiOlsicmVhZC10b2tlbiJdfSwiY2xvdWRncm91bmRjb250cm9sLWRldiI6eyJyb2xlcyI6WyJVc2VyIEFkbWluaXN0cmF0aW9uIl19LCJjbG91ZGdyb3VuZGNvbnRyb2wtc3RnIjp7InJvbGVzIjpbIlVzZXIgQWRtaW5pc3RyYXRpb24iXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5hbWUiOiJMdWFuIFRyYW4iLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJsdWFuLnRyYW5AYWR2YW5jZWRuYXZpZ2F0aW9uLmNvbSIsImdpdmVuX25hbWUiOiJMdWFuIiwiZmFtaWx5X25hbWUiOiJUcmFuIiwiZW1haWwiOiJsdWFuLnRyYW5AYWR2YW5jZWRuYXZpZ2F0aW9uLmNvbSJ9.AcQL9PbmCXznVTYtVBjcMHNakK1HbVp6Km2JTEPeacoPLg3sB0MmL5EYjshTdtiOZKULHQDeSazr7UfXv84eAblzAJEXt2qiTB5KneCRmF0HGHGM11V8ECOBOVPkHXoQp1lh50qUeH1nVG_x6svSf4acPG3ckROiKcLT0Oc-CsPHkedj';
+        if (jwtToken == '') {
+            jwtToken = 'eyJhbGciOiJFUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICItZTQ2Tm5iTXNRbG03ZjdUc0U2Rl9uSUkzakFOQ0NvN0ttNDJPMGFXdUxFIn0.eyJleHAiOjE2MjU3MzczMjAsImlhdCI6MTYyNTcwODUyMSwiYXV0aF90aW1lIjoxNjI1NzA4NTIwLCJqdGkiOiI0MjI4ZjI2MC1kN2EzLTQ5ZjgtOTg2My01ZjFiNzE3ZjRiZmEiLCJpc3MiOiJodHRwczovL2FwYWMuY2xvdWRncm91bmRjb250cm9sLmNvbS9hdXRoL3JlYWxtcy9jZ2NzLWRldiIsImF1ZCI6WyJicm9rZXIiLCJjbG91ZGdyb3VuZGNvbnRyb2wtZGV2Il0sInN1YiI6ImRjZmNjYjlhLTk3NTktNGVjYy1hZWQ5LTA5ZGI2YmI0NTk3MyIsInR5cCI6IkJlYXJlciIsImF6cCI6ImNsb3VkZ3JvdW5kY29udHJvbC1zdGciLCJzZXNzaW9uX3N0YXRlIjoiM2JjNmZjMTctYzRjYi00ZTUwLWFmMzYtMzc3ODNjMzg5M2IxIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJjZ2NfbWlzc2lvbl9tYW5hZ2VyIiwiY2djX3ZlaGljbGVfbWFuYWdlciIsImNnY192ZWhpY2xlX29wZXJhdG9yIiwiY2djX2FkbWluaXN0cmF0b3IiLCJjZ2NfdmVoaWNsZV9waWxvdCJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImJyb2tlciI6eyJyb2xlcyI6WyJyZWFkLXRva2VuIl19LCJjbG91ZGdyb3VuZGNvbnRyb2wtZGV2Ijp7InJvbGVzIjpbIlVzZXIgQWRtaW5pc3RyYXRpb24iXX0sImNsb3VkZ3JvdW5kY29udHJvbC1zdGciOnsicm9sZXMiOlsiVXNlciBBZG1pbmlzdHJhdGlvbiJdfX0sInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6Ikx1YW4gVHJhbiIsInByZWZlcnJlZF91c2VybmFtZSI6Imx1YW4udHJhbkBhZHZhbmNlZG5hdmlnYXRpb24uY29tIiwiZ2l2ZW5fbmFtZSI6Ikx1YW4iLCJmYW1pbHlfbmFtZSI6IlRyYW4iLCJlbWFpbCI6Imx1YW4udHJhbkBhZHZhbmNlZG5hdmlnYXRpb24uY29tIn0.AM2CkVSY6_QRgFAr3Fhsim8-3VfJ4Xjnn8hvq_M7MnfYfLaauybhOJLTtJuVm6T5R3a7_vrXkK7b1XOZyRad57BSAZhze5g5Vt_GnNm6QugZmAnwhOdvPpJ-5U8Q4hf5_HcH1PhTrRkN41XUwrZd7lEasfL5mNzyg6gpoNpwnK6YVCLs';
+            // const jwtToken = 'eyJhbGciOiJFUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICItZTQ2Tm5iTXNRbG03ZjdUc0U2Rl9uSUkzakFOQ0NvN0ttNDJPMGFXdUxFIn0.eyJleHAiOjE2MjUwNjI4NDEsImlhdCI6MTYyNTAzNDA0MiwiYXV0aF90aW1lIjoxNjI1MDM0MDQxLCJqdGkiOiJmMmZlMTBiYy1jMjUxLTQ0YjMtYjcyZC1kYTI3MGQ0MjEzNTkiLCJpc3MiOiJodHRwczovL2FwYWMuY2xvdWRncm91bmRjb250cm9sLmNvbS9hdXRoL3JlYWxtcy9jZ2NzLWRldiIsImF1ZCI6WyJicm9rZXIiLCJjbG91ZGdyb3VuZGNvbnRyb2wtZGV2Il0sInN1YiI6ImRjZmNjYjlhLTk3NTktNGVjYy1hZWQ5LTA5ZGI2YmI0NTk3MyIsInR5cCI6IkJlYXJlciIsImF6cCI6ImNsb3VkZ3JvdW5kY29udHJvbC1zdGciLCJzZXNzaW9uX3N0YXRlIjoiYWE1ODJhZGUtYjM3NS00OGU2LWI2MzQtZmE4N2MwZWRkMmQyIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJjZ2NfbWlzc2lvbl9tYW5hZ2VyIiwiY2djX3ZlaGljbGVfbWFuYWdlciIsImNnY192ZWhpY2xlX29wZXJhdG9yIiwiY2djX2FkbWluaXN0cmF0b3IiLCJjZ2NfdmVoaWNsZV9waWxvdCJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImJyb2tlciI6eyJyb2xlcyI6WyJyZWFkLXRva2VuIl19LCJjbG91ZGdyb3VuZGNvbnRyb2wtZGV2Ijp7InJvbGVzIjpbIlVzZXIgQWRtaW5pc3RyYXRpb24iXX0sImNsb3VkZ3JvdW5kY29udHJvbC1zdGciOnsicm9sZXMiOlsiVXNlciBBZG1pbmlzdHJhdGlvbiJdfX0sInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6Ikx1YW4gVHJhbiIsInByZWZlcnJlZF91c2VybmFtZSI6Imx1YW4udHJhbkBhZHZhbmNlZG5hdmlnYXRpb24uY29tIiwiZ2l2ZW5fbmFtZSI6Ikx1YW4iLCJmYW1pbHlfbmFtZSI6IlRyYW4iLCJlbWFpbCI6Imx1YW4udHJhbkBhZHZhbmNlZG5hdmlnYXRpb24uY29tIn0.AZ7BqkEnPeSww8sCuihGhPSLnDd3lMiEvkod8jf6HBFwiuUoSPg-1--6SrvyQC46Nprx2lfkoURagko4BkK_pNtqAdnkh6gRCmXEWThUjwMA_N0AulAhMYyUifrlvtUDhfjVA4bpyfGQil1Ttk6-PEqe4fO1O7_K3MA6OsXyadJ5htBb'
+        }
+        console.log(jwtToken);
 
         // const wssUrl = "wss://apac.cloudgroundcontrol.com/api/ws-vehicles";
         const wssUrl = "ws://localhost:9090/ws-vehicles";
+        // const wssUrl = "ws://uat.cloudgroundcontrol.com/api/ws-vehicles";
         this.stompClient = new Client({
             brokerURL: wssUrl,
-            debug: function (str: string) {
-                console.log('WS debug: ', str);
-            },
+            // debug: function (str: string) {
+            //     console.log('WS debug: ', str);
+            // },
             reconnectDelay: 5000,
             heartbeatIncoming: 4000,
             heartbeatOutgoing: 4000,
@@ -90,8 +107,8 @@ class App extends Component<{}, { connected: boolean }> {
         console.log("Disconnected");
     }
 
-    sendManualControl = (manualConrtol: any) => {
-        console.log("Sending manual control");
+    arm = () => {
+        console.log("Arm vehicle");
         const manualControl = {
             xAxis: 0.6,
             yAxis: 0.5,
@@ -108,7 +125,31 @@ class App extends Component<{}, { connected: boolean }> {
         this.stompClient.publish({
             destination: '/app/manual-control',
             binaryBody: encode,
-            headers: {"content-type": "application/octet-stream", "vehicleId": "luant-drone"}
+            headers: {"content-type": "application/octet-stream", "vehicleId": this.vehicleId}
+        });
+    }
+
+    sendManualControl = (manualConrtol: any) => {
+        console.log("Sending manual control");
+        const manualControl = {
+            xAxis: 0.61,
+            yAxis: 0.51,
+            zAxis: 0.31,
+            rAxis: 0.81,
+            cameraGimbalYaw: 1.0,
+            cameraGimbalPitch: 1.0,
+            commandType: CommandType.NAVIGATION
+        };
+        console.log(manualControl);
+        let encode = this.manualControlProtobuf.encode(manualControl).finish();
+        console.log(encode);
+        const decoded = this.manualControlProtobuf.decode(encode);
+        console.log(decoded);
+
+        this.stompClient.publish({
+            destination: '/app/manual-control',
+            binaryBody: encode,
+            headers: {"content-type": "application/octet-stream", "vehicleId": this.vehicleId}
         });
     }
 
@@ -116,13 +157,15 @@ class App extends Component<{}, { connected: boolean }> {
         // const destination = "/user/queue/operation/329bc8a162ae7208ae18976a9c4f95e9344b9ede18ec4126224b199e39dc1156";
         // const destination = "/user/queue/notifications";
         // adding prefix of /user won't work
-        const destination = "/topic/mock-positions"
-        // const destination = "/queue/operation/329bc8a162ae7208ae18976a9c4f95e9344b9ede18ec4126224b199e39dc1156";
+        // const destination = "/topic/mock-positions"
+        // const destination = "/user/queue/operation/dji_drone_0001";
+        // const destination = "/user/queue/operation/1a4ec7e5f7ce2d32ff125f5a57d52dbcb747ca8f7f17c514370584464dfe8df1";
+        const destination = "/user/queue/operation/1a4ec7e5f7ce2d32ff125f5a57d52dbcb747ca8f7f17c514370584464dfe8df1";
         console.log("Subscribing " + destination);
         console.log(this.stompClient);
         this.stompClient.subscribe(destination, (res: any) => {
             try {
-                const decoded = this.vehiclePositionProtobuf.decode(res.binaryBody);
+                const decoded = this.operationProtobuf.decode(res.binaryBody);
                 console.log(decoded);
             } catch (err) {
                 console.error(err);
@@ -147,8 +190,12 @@ class App extends Component<{}, { connected: boolean }> {
                 <button id="connect" className="btn btn-default" type="submit" ref={this.connectButtonRef}
                         onClick={this.connect} disabled={this.state.connected}>Connect
                 </button>
+                <input type="text" id="jwtToken" ref={this.jwtTokenInputRef}></input>
+                <br/>
                 <button id="disconnect" className="btn btn-default" type="submit" disabled={!this.state.connected}
                         ref={this.disconnectButtonRef} onClick={this.disconnect}>Disconnect
+                </button>
+                <button id="send_arm" className="btn btn-default" onClick={this.arm}>Arm
                 </button>
                 <button id="send" className="btn btn-default" onClick={this.sendManualControl}>Send Manual Control
                 </button>
@@ -157,6 +204,7 @@ class App extends Component<{}, { connected: boolean }> {
                     <option value="http://192.168.1.64:9090" defaultValue={"https://192.168.1.64:9090"}>Localhost
                     </option>
                     <option value="http://192.168.1.184:8082">Staging</option>
+                    <option value="https://uat.cloudgroundcontrol.com">UAT</option>
                 </select>
                 <p style={{padding: "10 10 10 10"}}>
                     <Gamepad ref={this.gamepadRef} disabled={false}>
--- src/Gamepad/Gamepad.tsx
+++ src/Gamepad/Gamepad.tsx
@@ -9,7 +9,8 @@ import { Client } from '@stomp/stompjs';
 
 enum CommandType {
     NAVIGATION,
-    ARM
+    ARM,
+    GIMBAL_CONTROL
 };
 
 class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z: number, r: number, commandType: string, isControllerConnected: boolean,
@@ -23,6 +24,9 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
     manualControlProtobuf: any;
 
     stompClient: any;
+
+    vehicleId: String = "luant-drone";//demo_px4_drone_0001//"1a4ec7e5f7ce2d32ff125f5a57d52dbcb747ca8f7f17c514370584464dfe8df1";
+    // vehicleId: String = "329bc8a162ae7208ae18976a9c4f95e9344b9ede18ec4126224b199e39dc1156";//"885ca38fe26b10ee60f7f7017413065dbcd41985ed8515ddbc27938eddcb64ba";//
 
     constructor(props: any) {
         super(props);
@@ -64,10 +68,11 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
     }
 
     connect = () => {
-        const jwtToken = 'eyJhbGciOiJFUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICItZTQ2Tm5iTXNRbG03ZjdUc0U2Rl9uSUkzakFOQ0NvN0ttNDJPMGFXdUxFIn0.eyJleHAiOjE2MTE3MzE4NTAsImlhdCI6MTYxMTcwMzA1MCwiYXV0aF90aW1lIjoxNjExNzAzMDQ5LCJqdGkiOiJmZDAyNGJhZS1hYjE1LTRhOTQtOGNjNC1lMTY3MjZkN2ZhZDgiLCJpc3MiOiJodHRwczovL2FwYWMuY2xvdWRncm91bmRjb250cm9sLmNvbS9hdXRoL3JlYWxtcy9jZ2NzLWRldiIsImF1ZCI6WyJicm9rZXIiLCJjbG91ZGdyb3VuZGNvbnRyb2wtZGV2Il0sInN1YiI6ImRjZmNjYjlhLTk3NTktNGVjYy1hZWQ5LTA5ZGI2YmI0NTk3MyIsInR5cCI6IkJlYXJlciIsImF6cCI6ImNsb3VkZ3JvdW5kY29udHJvbC1zdGciLCJzZXNzaW9uX3N0YXRlIjoiYzQ3MzdiM2EtOTM2YS00MGRjLWFmZmItOTFhMTkzNGFkYTAzIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sInJlc291cmNlX2FjY2VzcyI6eyJicm9rZXIiOnsicm9sZXMiOlsicmVhZC10b2tlbiJdfSwiY2xvdWRncm91bmRjb250cm9sLWRldiI6eyJyb2xlcyI6WyJVc2VyIEFkbWluaXN0cmF0aW9uIl19LCJjbG91ZGdyb3VuZGNvbnRyb2wtc3RnIjp7InJvbGVzIjpbIlVzZXIgQWRtaW5pc3RyYXRpb24iXX19LCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsIm5hbWUiOiJMdWFuIFRyYW4iLCJwcmVmZXJyZWRfdXNlcm5hbWUiOiJsdWFuLnRyYW5AYWR2YW5jZWRuYXZpZ2F0aW9uLmNvbSIsImdpdmVuX25hbWUiOiJMdWFuIiwiZmFtaWx5X25hbWUiOiJUcmFuIiwiZW1haWwiOiJsdWFuLnRyYW5AYWR2YW5jZWRuYXZpZ2F0aW9uLmNvbSJ9.AQAlKDiZlwI74QU4O-khoFBclHYuX6jWUQ0vyVVudCp0OXib4Ner53Xwx7PNSSF2VNmPRpXl_FZNNCajy38lpJ_MASI4FcfY_CFPvnm5nW3bCHda0ydVx3jNR2q4iIleRaiMvX1jYHlsYv2R9VZOKwRU8YlRcwl5IB84DWJzKZbYbjNJ';
+        const jwtToken = 'eyJhbGciOiJFUzUxMiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICItZTQ2Tm5iTXNRbG03ZjdUc0U2Rl9uSUkzakFOQ0NvN0ttNDJPMGFXdUxFIn0.eyJleHAiOjE2MjU3MzczMjAsImlhdCI6MTYyNTcwODUyMSwiYXV0aF90aW1lIjoxNjI1NzA4NTIwLCJqdGkiOiI0MjI4ZjI2MC1kN2EzLTQ5ZjgtOTg2My01ZjFiNzE3ZjRiZmEiLCJpc3MiOiJodHRwczovL2FwYWMuY2xvdWRncm91bmRjb250cm9sLmNvbS9hdXRoL3JlYWxtcy9jZ2NzLWRldiIsImF1ZCI6WyJicm9rZXIiLCJjbG91ZGdyb3VuZGNvbnRyb2wtZGV2Il0sInN1YiI6ImRjZmNjYjlhLTk3NTktNGVjYy1hZWQ5LTA5ZGI2YmI0NTk3MyIsInR5cCI6IkJlYXJlciIsImF6cCI6ImNsb3VkZ3JvdW5kY29udHJvbC1zdGciLCJzZXNzaW9uX3N0YXRlIjoiM2JjNmZjMTctYzRjYi00ZTUwLWFmMzYtMzc3ODNjMzg5M2IxIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6WyIqIl0sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJjZ2NfbWlzc2lvbl9tYW5hZ2VyIiwiY2djX3ZlaGljbGVfbWFuYWdlciIsImNnY192ZWhpY2xlX29wZXJhdG9yIiwiY2djX2FkbWluaXN0cmF0b3IiLCJjZ2NfdmVoaWNsZV9waWxvdCJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImJyb2tlciI6eyJyb2xlcyI6WyJyZWFkLXRva2VuIl19LCJjbG91ZGdyb3VuZGNvbnRyb2wtZGV2Ijp7InJvbGVzIjpbIlVzZXIgQWRtaW5pc3RyYXRpb24iXX0sImNsb3VkZ3JvdW5kY29udHJvbC1zdGciOnsicm9sZXMiOlsiVXNlciBBZG1pbmlzdHJhdGlvbiJdfX0sInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmFtZSI6Ikx1YW4gVHJhbiIsInByZWZlcnJlZF91c2VybmFtZSI6Imx1YW4udHJhbkBhZHZhbmNlZG5hdmlnYXRpb24uY29tIiwiZ2l2ZW5fbmFtZSI6Ikx1YW4iLCJmYW1pbHlfbmFtZSI6IlRyYW4iLCJlbWFpbCI6Imx1YW4udHJhbkBhZHZhbmNlZG5hdmlnYXRpb24uY29tIn0.AM2CkVSY6_QRgFAr3Fhsim8-3VfJ4Xjnn8hvq_M7MnfYfLaauybhOJLTtJuVm6T5R3a7_vrXkK7b1XOZyRad57BSAZhze5g5Vt_GnNm6QugZmAnwhOdvPpJ-5U8Q4hf5_HcH1PhTrRkN41XUwrZd7lEasfL5mNzyg6gpoNpwnK6YVCLs';
 
         // const wssUrl = "wss://apac.cloudgroundcontrol.com/api/ws-vehicles";
         const wssUrl = "ws://localhost:9090/ws-vehicles";
+        // const wssUrl = "wss://uat.cloudgroundcontrol.com/api/ws-vehicles";
         this.stompClient = new Client({
             brokerURL: wssUrl,
             debug: function (str: string) {
@@ -96,7 +101,7 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
         this.stompClient.connectHeaders = {Authorization: jwtToken};
         this.stompClient.activate();
 
-        this.subscribe();
+        // this.subscribe();
     }
 
     subscribe = () => {
@@ -137,7 +142,8 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
         // const decoded = this.manualControlProtobuf.decode(encode);
         // console.log(decoded);
 
-        this.stompClient.publish({ destination:'/app/manual-control', binaryBody: encode, headers:  {"content-type": "application/octet-stream", "vehicleId": "luant-drone"} });
+        console.log(manualConrtol);
+        this.stompClient.publish({ destination:'/app/manual-control', binaryBody: encode, headers:  {"content-type": "application/octet-stream", "vehicleId": this.vehicleId} });
     }
 
     componentDidMount() {
@@ -203,6 +209,10 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
                     "yAxis": this.state.y,
                     "zAxis": this.state.z,
                     "rAxis": this.state.r,
+
+                    "cameraGimbalYaw": this.state.x,
+                    "cameraGimbalPitch": this.state.y,
+
                     "commandType": CommandType.ARM
                 };
 
@@ -216,6 +226,20 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
 
     sendRequest = () => {
         const shouldSendRequest = !this.isGamepadDisabled() && this.state.isWebSocketConnected === true
+        var yaw = 0;//-this.state.y * 5
+        var yawUnit = 5;
+        if (this.state.y < 0) {
+            yaw = -yawUnit;
+        } else if (this.state.y >0){
+            yaw = yawUnit;
+        }
+        var pitch = 0; //this.state.x * 5
+        var pitchUnit = 5;
+        if (this.state.x < 0) {
+            pitch = -pitchUnit;
+        } else if (this.state.x > 0) {
+            pitch = pitchUnit;
+        }
         if (shouldSendRequest) {
 
             const data = {
@@ -223,12 +247,17 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
                 "yAxis": this.state.y,
                 "zAxis": this.state.z,
                 "rAxis": this.state.r,
+
+                "cameraGimbalYaw": -yaw * 3,
+                "cameraGimbalPitch": pitch,
+
                 "commandType": CommandType.NAVIGATION
             };
 
+            console.log(data.cameraGimbalYaw);
             this.sendManualControl(data);
         }
-    }
+    };
 
     webRightJoystickHandler = (event: IJoystickUpdateEvent) => {
         const y = this.normalizeMousePositionToWebJoystickValue((event.x == null) ? 0 : event.x);
@@ -237,7 +266,7 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
             x: x,
             y: y
         });
-    }
+    };
 
     webLeftJoystickHandler = (event: IJoystickUpdateEvent) => {
         const z = this.normalizeMousePositionToWebJoystickValue((event.y == null) ? 0 : event.y);
@@ -246,7 +275,7 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
             z,
             r
         })
-    }
+    };
 
     normalizeMousePositionToWebJoystickValue = (mousePosition: number) => {
         const maxMouseValue = this.joystickSize / 2;
@@ -257,7 +286,7 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
         const result = 2 * ((mousePosition) / (max - min));
 
         return result;
-    }
+    };
 
     isGamepadDisabled = () => {
         let disabled = this.props.disabled || !this.state.isControllerConnected;
@@ -265,7 +294,7 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
             disabled = false;
         }
         return disabled;
-    }
+    };
 
     render() {
         return (
@@ -322,7 +351,7 @@ class Gamepad extends Component<{ disabled: boolean }, { x: number, y: number, z
     }
 
     // Convenient variables + methods
-    manualControlUrl: string = "http://localhost:9090/manual-control";
+    manualControlUrl: string = "https://uat.cloudgroundcontrol.com"; //http://localhost:9090/manual-control";
 
     changeManualControlUrl = (newUrl: string) => {
         this.manualControlUrl = newUrl;
